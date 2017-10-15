4241a3d32ad8e3cc3faff050cccd7f7bd59d7679
msaeed5 <mahmoud.saeed.ext@valeo.com>
2017-10-15 18:07:31 +0200

    complete the loop

--- model.py
+++ model.py
@@ -22,18 +22,18 @@ class Net(nn.Module):
         self.fc2 = nn.Linear(64 * 2, 64)
         self.fc3 = nn.Linear(64, 64)
         self.fc4 = nn.Linear(64, 64)
-        #end of visual encoder
+        # end of visual encoder
 
-        #dynamic predictor modules
-        self.self_cores={}
+        # dynamic predictor modules
+        self.self_cores = {}
         for i in range(3):
-            self.self_cores[i]=[]
+            self.self_cores[i] = []
             self.self_cores[i].append(nn.Linear(64, 64))
             self.self_cores[i].append(nn.Linear(64, 64))
-        self.rel_cores={}
+        self.rel_cores = {}
         for i in range(3):
-            self.rel_cores[i]=[]
-            self.rel_cores[i].append(nn.Linear(64*2, 64))
+            self.rel_cores[i] = []
+            self.rel_cores[i].append(nn.Linear(64 * 2, 64))
             self.rel_cores[i].append(nn.Linear(64, 64))
             self.rel_cores[i].append(nn.Linear(64, 64))
 
@@ -46,44 +46,46 @@ class Net(nn.Module):
         self.out = {}
         for i in range(3):
             self.out[i] = []
-            self.out[i].append(nn.Linear(64+64, 64))
+            self.out[i].append(nn.Linear(64 + 64, 64))
             self.out[i].append(nn.Linear(64, 64))
+        self.aggregator1 = nn.Linear(64 * 3, 64)
+        self.aggregator2 = nn.Linear(64, 64)
 
-    def core(self,s,core_idx):
-        objects = torch.chunk(s,3,1)
+    def core(self, s, core_idx):
+        objects = torch.chunk(s, 3, 1)
 
-        s_reshaped=s.view(-1,64)
-        self_sd_h1=F.relu(self.self_cores[core_idx][0](s_reshaped))
-        self_sd_h2= self.self_cores[core_idx][1](self_sd_h1) + self_sd_h1
-        self_dynamic=self_sd_h2.view(-1,3,64)
+        s_reshaped = s.view(-1, 64)
+        self_sd_h1 = F.relu(self.self_cores[core_idx][0](s_reshaped))
+        self_sd_h2 = self.self_cores[core_idx][1](self_sd_h1) + self_sd_h1
+        self_dynamic = self_sd_h2.view(-1, 3, 64)
 
-        rel_combination=[]
+        rel_combination = []
         for i in range(6):
             row_idx = int(i / (2));
             col_idx = int(i % (2));
-            rel_combination.append(torch.cat([objects[row_idx], objects[col_idx]],1))
-        rel_combination=torch.cat(rel_combination)
+            rel_combination.append(torch.cat([objects[row_idx], objects[col_idx]], 1))
+        rel_combination = torch.cat(rel_combination)
 
         rel_sd_h1 = F.relu(self.rel_cores[core_idx][0](rel_combination))
-        rel_sd_h2 = F.relu(self.rel_cores[core_idx][1](rel_sd_h1)+rel_sd_h1)
+        rel_sd_h2 = F.relu(self.rel_cores[core_idx][1](rel_sd_h1) + rel_sd_h1)
         rel_sd_h3 = self.self_cores[core_idx][2](rel_sd_h2) + rel_sd_h2
-        rel_objects=torch.chunk(rel_sd_h3,6)
-        obj1=rel_objects[0]+rel_objects[1]
-        obj2=rel_objects[2]+rel_objects[3]
-        obj3=rel_objects[4]+rel_objects[5]
-        rel_dynamic=torch.stack([obj1,obj2,obj3],1)
-        dynamic_pred=self_dynamic+rel_dynamic
-        dynamic_pred=dynamic_pred.view(-1,64)
+        rel_objects = torch.chunk(rel_sd_h3, 6)
+        obj1 = rel_objects[0] + rel_objects[1]
+        obj2 = rel_objects[2] + rel_objects[3]
+        obj3 = rel_objects[4] + rel_objects[5]
+        rel_dynamic = torch.stack([obj1, obj2, obj3], 1)
+        dynamic_pred = self_dynamic + rel_dynamic
+        dynamic_pred = dynamic_pred.view(-1, 64)
         aff1 = F.relu(self.affector[core_idx][0](dynamic_pred))
         aff2 = F.relu(self.affector[core_idx][1](aff1) + aff1)
         aff3 = self.affector[core_idx][2](aff2) + rel_sd_h2
-        aff3=aff3.view(-1,3,64)
-        aff_s=torch.cat([aff3,s],2)
-        aff_s=aff_s.view(-1,64+64)
+        aff3 = aff3.view(-1, 3, 64)
+        aff_s = torch.cat([aff3, s], 2)
+        aff_s = aff_s.view(-1, 64 + 64)
 
         out1 = F.relu(self.out[core_idx][0](aff_s))
         out2 = self.out[core_idx][1](out1) + out1
-        out2=out2.view(-1,3,64)
+        out2 = out2.view(-1, 3, 64)
 
         return out2
 
@@ -124,20 +126,23 @@ class Net(nn.Module):
 
         diff_pairs = torch.cat([pair1, pair2, pair3, pair4])
         diff_pairs = diff_pairs.view(-1, 64 * 2)
-        shared_h1=F.relu(self.fc2(diff_pairs))
-        shared_h2=F.relu(self.fc2(shared_h1)+shared_h1)
-        shared_h3=self.fc2(diff_pairs)+shared_h2
-        state_codes=shared_h3.view(-1,3,64)
+        shared_h1 = F.relu(self.fc2(diff_pairs))
+        shared_h2 = F.relu(self.fc2(shared_h1) + shared_h1)
+        shared_h3 = self.fc2(diff_pairs) + shared_h2
+        state_codes = shared_h3.view(-1, 3, 64)
         s1, s2, s3, s4 = torch.chunk(state_codes, 4)
-
+        rolls = []
         for i in range(20):
-            pass
-
-
-
-
-
-
+            c1 = self.core(s1, 0)
+            c2 = self.core(s2, 1)
+            c3 = self.core(s3, 2)
+            all_c = torch.cat([c1, c2, c3], 2)
+            all_c = all_c.view(-1, 64 * 3)
+            aggregator1 = F.relu(self.aggregator1(all_c))
+            aggregator2 = self.aggregator2(aggregator1)
+            aggregator2 = aggregator2.view(-1, 3, 64)
+            rolls.append(aggregator2)
+            s1, s2, s3, s4 = s2, s3, s4, aggregator2
         return x
 
     def num_flat_features(self, x):
